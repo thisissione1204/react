eb2e035efd34e56306c8b9733d8aeb4df641a61d
thisissione1204 <thisissione1204@gmail.com>
2026-04-23 16:03:16 -0300

    ok

--- tarefas-app/src/App.js
+++ tarefas-app/src/App.js
@@ -1,9 +1,49 @@
+import { useState, useEffect } from "react";
+
 
 function App() {
+
+  const [input, setInput] = useState("");
+
+  const [tarefas, setTarefas] = useState([]);
+
+
+  function enviarTarefa(e){
+
+    e.preventDefault();
+
+    setTarefas([...tarefas, input]);
+    setInput('');
+
+  }
+
+  
+
+
   return (
     <div>
-
       <h1>Lista de Tarefas</h1>
+
+      <form onSubmit={enviarTarefa}>
+        
+        <label>Nome da tarefa: </label> 
+        <br/>
+        <input type="text" placeholder="Digite a tarefa"
+        
+        value={input}
+        onChange={(e)=>setInput(e.target.value)}
+        />
+        <br/>
+        <br/>
+
+        <button>Registar</button>
+      </form>
+       <br/>
+      <ul>
+          {tarefas.map(index=>(
+            <li>{index}</li>
+          ))}
+      </ul>
     
     </div>
   );
